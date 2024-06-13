f4ba9d1c1a87efcb81b8150cfb1eb0f900f76e8a
rijumas <mauricio3parker@gmail.com>
2024-06-13 15:32:16 -0500

    Cambios moodle

--- initdatabase.py
+++ initdatabase.py
@@ -1,8 +1,8 @@
 import sqlite3
 from sqlite3 import Error
+import bcrypt
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
     conn = None
     try:
         conn = sqlite3.connect(db_file)
@@ -12,105 +12,79 @@ def create_connection(db_file):
     return conn
 
 def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement """
     try:
         c = conn.cursor()
         c.execute(create_table_sql)
     except Error as e:
         print(e)
 
-def insert_missions(conn):
-    """ insert missions into the missions table """
-    missions = [
-    ('Pamplona Alta - 2018 (video)', '2018-01-01', '2018-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Chachapoyas - 2017 (photos)', '2017-01-01', '2017-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Ayaviri - 2017 (photos)', '2017-01-01', '2017-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Pamplona Alta - 2014 (photos)', '2014-01-01', '2014-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Pamplona Alta - 2012 (photos)', '2012-01-01', '2012-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Ayaviri - 2012 (video)', '2012-01-01', '2012-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Pamplona Alta - 2011 (photos)', '2011-01-01', '2011-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Ayaviri - 2011 (photos)', '2011-01-01', '2011-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Pamplona Alta - 2010 (video)', '2010-01-01', '2010-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Chincha, Ica - 2008 (slide show)', '2008-01-01', '2008-12-31', 'Itinerary Placeholder', 0.0, 'Cost Description Placeholder', 'Contact Placeholder'),
-    ('Ayaviri-2023', '2023-09-22', '2023-09-30', 
-     '''September 22 - Depart US\n
-        September 23 - Arrive Cusco/acclimate/Mandatory group meeting & dinner\n
-        September 24 - Day trip as a mission group/get to know each other\n
-        September 25 - Transfer to Ayaviri, set up clinic\n
-        September 26 - Medical Mission\n
-        September 27 - Medical Mission\n
-        September 28 - Medical Mission\n
-        September 29 - Medical Mission half day/home visits and pack up mission site\n
-        September 30 - Depart for home''', 
-     925.0, 
-     '''All hotel accommodations with breakfast (from September 23rd to 30th), 
-        transfer to and from the airport on September 23rd and 30th (Cuzco and Juliaca only), 
-        team day trip and dinner in Cuzco, 2 T-shirts, ground transportation (Cuzco – Ayaviri), 
-        all meals in Ayaviri, participant contribution for medications, supplies and year around continue care 
-        for the population we serve.\n\n
-        It doesn't include: Personal expenses, voluntary tips, meals, airfare, nor anything not mentioned above. 
-        Current roundtrip air from major cities in the US to Cusco is approximately $2,000.\n\n
-        A USD 400.00 deposit is required with your application. 
-        The remaining balance is required by August 15th, 2023.''', 
-     'Contact: Katy Brant\nMission Coordinator\n(303) 476-1358')
-]
-
-
-    sql = ''' INSERT INTO missions(Name, Date_to_begin, Date_to_end, Itinerary, Cost, mission_cost_description, Contact)
-              VALUES(?,?,?,?,?,?,?) '''
-    cur = conn.cursor()
-    cur.executemany(sql, missions)
-    conn.commit()
-
 def insert_admin(conn):
-    """ insert an admin user into the users table """
-    admin = ('Admin', 'User', '1970-01-01', 'Country Placeholder', 'admin@example.com', 1, 'adminpassword', 1)
-    sql = ''' INSERT INTO users(Name, Surname, Birthdate, Country, Email, Approved, Password, is_admin)
-              VALUES(?,?,?,?,?,?,?,?) '''
+    name = 'Admin'
+    surname = 'User'
+    birthdate = '1970-01-01'
+    country = 'Country Placeholder'
+    email = 'admin@example.com'
+    password = 'adminpassword'
+    is_admin = 1
+    
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    
+    admin = (name, surname, birthdate, country, email, hashed_password, is_admin)
+    
+    sql = ''' INSERT INTO users(Name, Surname, Birthdate, Country, Email, Password, is_admin)
+              VALUES(?,?,?,?,?,?,?) '''
+    
     cur = conn.cursor()
     cur.execute(sql, admin)
     conn.commit()
+
+def create_posts_table(conn):
+    try:
+        sql_create_posts_table = """ CREATE TABLE IF NOT EXISTS posts (
+                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        title TEXT,
+                                        subtitle TEXT,
+                                        content TEXT,
+                                        image TEXT,
+                                        section TEXT NOT NULL,
+                                        `order` INTEGER DEFAULT 0,
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                    ); """
+        c = conn.cursor()
+        c.execute(sql_create_posts_table)
+        
+        # Add the order column if it doesn't exist
+        c.execute("PRAGMA table_info(posts);")
+        columns = [column[1] for column in c.fetchall()]
+        if 'order' not in columns:
+            c.execute("ALTER TABLE posts ADD COLUMN `order` INTEGER DEFAULT 0;")
+        conn.commit()
+    except Error as e:
+        print(f"Error occurred: {e}")
 
 def main():
     database = "database.db"
 
     sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
-                                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        Name TEXT NOT NULL,
-                                        Surname TEXT NOT NULL,
-                                        Birthdate TEXT NOT NULL,
-                                        Country TEXT NOT NULL,
-                                        Email TEXT NOT NULL,
-                                        Approved INTEGER NOT NULL,
-                                        Password TEXT NOT NULL,
-                                        is_admin INTEGER NOT NULL
-                                    ); """
-
-    sql_create_missions_table = """ CREATE TABLE IF NOT EXISTS missions (
                                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                                     Name TEXT NOT NULL,
-                                    Date_to_begin DATE,
-                                    Date_to_end DATE,
-                                    Itinerary TEXT,
-                                    Cost FLOAT,
-                                    mission_cost_description TEXT,
-                                    Contact TEXT
-                                );"""
+                                    Surname TEXT NOT NULL,
+                                    Birthdate TEXT,
+                                    Country TEXT,
+                                    Email TEXT NOT NULL,
+                                    Password TEXT NOT NULL,
+                                    is_admin INTEGER NOT NULL DEFAULT 0
+                                ); """
 
-    # create a database connection
     conn = create_connection(database)
+
     if conn is not None:
-        # create users table
         create_table(conn, sql_create_users_table)
-        # create missions table
-        create_table(conn, sql_create_missions_table)
-        # insert missions
-        insert_missions(conn)
-        # insert admin user
         insert_admin(conn)
+        create_posts_table(conn)
         conn.close()
     else:
-        print("Error! cannot create the database connection.")
+        print("Error! Cannot create the database connection.")
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,51 +1,60 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 import bcrypt
 import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'app.OsJoMeMi'
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
+
+@app.context_processor
+def utility_processor():
+    def is_admin():
+        if 'user_id' in session:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT is_admin FROM users WHERE id = ?", (session['user_id'],))
+            user = cursor.fetchone()
+            conn.close()
+            if user and user[0] == 1:
+                return True
+        return False
+    
+    return dict(is_admin=is_admin)
+
+def is_admin():
+        if 'user_id' in session:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT is_admin FROM users WHERE id = ?", (session['user_id'],))
+            user = cursor.fetchone()
+            conn.close()
+            if user and user[0] == 1:
+                return True
+        return False
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/create_account', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        name = request.form['name']
-        surname = request.form['surname']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
-        birthdate = request.form['birthdate']
-        country = request.form['country']
-
-        if password != confirm_password:
-            return render_template("register.html", error="Passwords do not match")
-        # Hash the password
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE Email = ?", (email,))
-        existing_user = cursor.fetchone()
-        if existing_user:
-            conn.close()
-            return render_template("register.html", error="Email already registered")
-        cursor.execute("INSERT INTO users (Name, Surname, Birthdate, Country, Email, Password, Approved) VALUES (?, ?, ?, ?, ?, ?, ?)", (name, surname, birthdate, country, email, hashed_password, 0))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('login'))
-
-    return render_template("register.html")
+def get_posts_by_section(section):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM posts WHERE section = ? ORDER BY `order` ASC, created_at DESC", (section,))
+    posts = cursor.fetchall()
+    conn.close()
+    return posts
 
 @app.route('/goals_and_services')
 def goalsServices():
-    return render_template("goals_services.html")
+    posts = get_posts_by_section('goals_and_services')
+    return render_template("goals_services.html", posts=posts)
 
 @app.route('/next_mission')
 def nextMission():
-    return render_template("next_mission.html")
+    posts = get_posts_by_section('next_mission')
+    return render_template("next_mission.html", posts=posts)
 
 @app.route('/donations')
 def donations():
@@ -53,59 +62,18 @@ def donations():
 
 @app.route('/contact_us')
 def contactUs():
-    return render_template("contact_us.html")
+    posts = get_posts_by_section('contact_us')
+    return render_template("contact_us.html", posts=posts)
 
 @app.route('/photo_videos')
 def photoVideos():
-    return render_template("photo_videos.html")
-
-def getMissions():
-    conn = sqlite3.connect('database.db')  # Connect to the database
-    cursor = conn.cursor()
-    
-    # Execute the query to fetch all missions
-    cursor.execute("SELECT id, Name, Date_to_begin, Date_to_end, Itinerary, Cost, mission_cost_description, Contact FROM missions")
-    
-    # Fetch all rows from the executed query
-    missions = cursor.fetchall()
-    
-    # Close the connection
-    conn.close()
-    
-    return missions
-
-def getMission(mission_name):
-    # Connect to your database (replace 'database.db' with your actual database file)
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
-    # Use parameterized query to prevent SQL injection
-    cursor.execute("""
-        SELECT id, Name, Date_to_begin, Date_to_end, Itinerary, Cost, mission_cost_description, Contact
-        FROM missions
-        WHERE Name = ?
-    """, (mission_name,))
-
-    mission = cursor.fetchone()
-    conn.close()
-    return mission
-
-@app.route('/mission_information/<mission_name>')
-def missionInformation(mission_name):
-    mission_ = getMission(mission_name)
-    
-    # Get image paths dynamically based on the mission_name
-    image_dir = f'static/{mission_name}_images'
-    if os.path.exists(image_dir):
-        images = [os.path.join(image_dir, img).replace('\\', '/') for img in os.listdir(image_dir) if img.endswith('.png')]
-    else:
-        images = []
-    
-    return render_template('mission_information.html', mission=mission_, images=images)
+    posts = get_posts_by_section('photo_videos')
+    return render_template("photo_videos.html", posts=posts)
 
 @app.route('/terms_and_conditions')
 def termsAndConditions():
-    return render_template('terms.html')
+    posts = get_posts_by_section('terms_and_conditions')
+    return render_template('terms.html', posts=posts)
 
 @app.route('/about_peru')
 def aboutPeru():
@@ -113,8 +81,8 @@ def aboutPeru():
 
 @app.route('/previous_missions')
 def previousMissions():
-    listMissions = getMissions()
-    return render_template('previous_missions.html', missions=listMissions)
+    posts = get_posts_by_section('previous_missions')
+    return render_template("previous_missions.html", posts=posts)
 
 @app.route('/login_account', methods=['GET', 'POST'])
 def login():
@@ -126,7 +94,7 @@ def login():
         cursor.execute("SELECT * FROM users WHERE Email = ?", (email,))
         user = cursor.fetchone()
         conn.close()
-        if user and bcrypt.checkpw(password.encode('utf-8'), user[7]):
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[6]):
             session['user_id'] = user[0]
             session['user_name'] = user[1]
             return redirect(url_for('index'))
@@ -134,9 +102,164 @@ def login():
             return render_template("login.html", error="Invalid username or password")
     return render_template("login.html")
 
+@app.route('/create_post', methods=['GET', 'POST'])
+def createPost():
+    if 'user_id' not in session or not is_admin():
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        title = request.form.get('title', None)
+        subtitle = request.form.get('subtitle', None)
+        content = request.form.get('content', None)
+        image_file = request.files['image'] if 'image' in request.files else None
+        section = request.form.get('section', None)
+        
+        if not (title or subtitle or content or image_file):
+            return render_template('create_post.html', error="Please provide valid content.")
+        
+        if image_file:
+            filename = secure_filename(image_file.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image_file.save(image_path)
+            image_url = f'static/uploads/{filename}'
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO posts (title, subtitle, content, image, section) VALUES (?, ?, ?, ?, ?)",
+                           (title, subtitle, content, image_url, section))
+            conn.commit()
+            conn.close()
+        else:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO posts (title, subtitle, content, image, section) VALUES (?, ?, ?, ?, ?)",
+                           (title, subtitle, content, None, section))
+            conn.commit()
+            conn.close()
+        
+        return redirect(url_for('index'))
+    
+    return render_template('create_post.html')
+
+@app.route('/edit_content', methods=['GET', 'POST'])
+def editContent():
+    if 'user_id' not in session or not is_admin():
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        section = request.form.get('section', None)
+        if section:
+            posts = get_posts_by_section(section)
+            return render_template('edit_content.html', posts=posts, section=section)
+    
+    return render_template('select_section.html')
+
+@app.route('/edit_posts', methods=['GET', 'POST'])
+def editPosts():
+    if 'user_id' not in session or not is_admin():
+        return redirect(url_for('index'))
+    
+    if request.method == 'POST':
+        post_id = request.form.get('post_id')
+        action = request.form.get('action')
+
+        if action == 'edit':
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM posts WHERE id = ?", (post_id,))
+            post = cursor.fetchone()
+            conn.close()
+            
+            if post:
+                return render_template('edit_post.html', post=post)
+            else:
+                flash('Post not found', 'error')
+                return redirect(request.referrer or url_for('index'))
+
+        elif action == 'save':
+            title = request.form.get('title')
+            subtitle = request.form.get('subtitle')
+            content = request.form.get('content')
+            image_file = request.files.get('image')
+            
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            
+            if image_file:
+                filename = secure_filename(image_file.filename)
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                image_file.save(image_path)
+                image_url = f'static/uploads/{filename}'
+                cursor.execute("""
+                    UPDATE posts SET title = ?, subtitle = ?, content = ?, image = ? WHERE id = ?
+                """, (title, subtitle, content, image_url, post_id))
+            else:
+                cursor.execute("""
+                    UPDATE posts SET title = ?, subtitle = ?, content = ? WHERE id = ?
+                """, (title, subtitle, content, post_id))
+            
+            conn.commit()
+            conn.close()
+
+            flash('Post updated successfully', 'success')
+            return redirect(url_for('index'))
+
+        elif action == 'delete':
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("SELECT image FROM posts WHERE id = ?", (post_id,))
+            post = cursor.fetchone()
+            
+            if post:
+                image_path = post[0]
+                cursor.execute("DELETE FROM posts WHERE id = ?", (post_id,))
+                conn.commit()
+                conn.close()
+                
+                if image_path:
+                    full_image_path = os.path.join(app.root_path, image_path)
+                    if os.path.exists(full_image_path):
+                        os.remove(full_image_path)
+                
+                flash('Post deleted successfully', 'success')
+            else:
+                flash('Post not found', 'error')
+            
+            return redirect(request.referrer or url_for('index'))
+        elif action == 'move_up' or action == 'move_down':
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+
+            cursor.execute("SELECT section, `order` FROM posts WHERE id = ?", (post_id,))
+            post = cursor.fetchone()
+            section, current_order = post[0], post[1]
+
+            if action == 'move_up':
+                new_order = current_order - 1
+            elif action == 'move_down':
+                new_order = current_order + 1
+
+            cursor.execute("""
+                UPDATE posts SET `order` = ? WHERE section = ? AND `order` = ?
+            """, (current_order, section, new_order))
+            cursor.execute("""
+                UPDATE posts SET `order` = ? WHERE id = ?
+            """, (new_order, post_id))
+
+            conn.commit()
+            conn.close()
+
+            flash('Post order updated successfully', 'success')
+            return redirect(request.referrer or url_for('index'))
+        
+    return redirect(request.referrer or url_for('index'))
+
+        
+        
+    
+
 @app.route('/logout')
 def logout():
-    session.pop('user_id', None)  # Elimina el ID del usuario de la sesión
+    session.pop('user_id', None)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
